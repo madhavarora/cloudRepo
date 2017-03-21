f21129d268f8fe845752519df5d478ce260cb61d
1306735 <1306735@01HW905415.India.TCS.com>
2017-03-21 07:39:47 +0530

    First commit from eclipse

---
+++ contacts2/src/main/java/services/ContactsService.java
@@ -0,0 +1,16 @@
+package services;
+
+/*Copyright © 2014 Oracle and/or its affiliates. All rights reserved */
+
+import javax.jws.*;
+
+
+@WebService
+public class ContactsService {
+	
+	@WebMethod
+	public void getContact() {
+		return;
+	}
+
+}
---
+++ contacts2/src/main/webapp/index.jsp
@@ -0,0 +1,6 @@
+<html>
+<!-- /*Copyright © 2014 Oracle and/or its affiliates. All rights reserved */ -->
+<body>
+<h2>PTS says: "Hello Cloud World!"</h2>
+</body>
+</html>
